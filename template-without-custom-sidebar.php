192eb48e67ac0d92063bbdeef41f1e33b4f3a15d
Björn Gerhart <airbjorn@users.noreply.github.com>
2020-01-04 13:00:38 +0100

    Add files via upload
    new template disabling the custom sidebar

---
+++ template-without-custom-sidebar.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Template Name: Without Custom Sidebar
+ *
+ * Description: A custom template not showing Custom Sidebar at all
+ *
+ * @package Twenty Seventeen Child for dcb.org
+ */
+
+get_header(); ?>
+
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			while ( have_posts() ) :
+				the_post();
+
+				get_template_part( 'template-parts/page/content', 'page-without-custom-sidebar' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
+
+<?php
+get_footer();
